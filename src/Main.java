169fa57c4b80b57c6a38cfb2559dc39606329adf
Westalgie <lexa.zaytsev@gmail.com>
2022-11-16 18:08:56 +0300

    exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        MyVector a = new MyVector(5, 6, 8);
-        MyVector b = new MyVector(5, 10, 15);
+        MyVector a = new MyVector(5, 6, 8, -1, 0, 2);
+        MyVector b = new MyVector(5, 10, 15, 20, 25, 30);
 
         System.out.print("Длина вектора a: ");
         System.out.println(a.GetVectorLength());
--- src/MyVector.java
+++ src/MyVector.java
@@ -1,30 +1,45 @@
 import static java.lang.Math.sqrt;
 
 public class MyVector {
-    private int[] coordinates;
+    private final int[] coordinates;
 
     public MyVector(int ... coordinates) {
         this.coordinates = coordinates;
     }
 
     public MyVector AddVector(MyVector vector) {
-        for(int i = 0; i < this.coordinates.length; i++) {
-            this.coordinates[i] = this.coordinates[i] + vector.coordinates[i];
+        try {
+            for (int i = 0; i < this.coordinates.length; i++) {
+                this.coordinates[i] = this.coordinates[i] + vector.coordinates[i];
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
         }
         return this;
     }
 
     public MyVector SubtractVector(MyVector vector) {
-        for(int i = 0; i < this.coordinates.length; i++) {
-            this.coordinates[i] = this.coordinates[i] - vector.coordinates[i];
+        try {
+            for (int i = 0; i < this.coordinates.length; i++) {
+                this.coordinates[i] = this.coordinates[i] - vector.coordinates[i];
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
         }
         return this;
     }
 
     static int ScalarMultiplication(MyVector a, MyVector b) {
         int result = 0;
-        for(int i = 0; i < a.coordinates.length; i++) {
-            result += a.coordinates[i] * b.coordinates[i];
+        try {
+            for (int i = 0; i < a.coordinates.length; i++) {
+                result += a.coordinates[i] * b.coordinates[i];
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
         }
         return result;
     }
@@ -45,16 +60,16 @@ public class MyVector {
 
     public double GetVectorLength() {
         int result = 0;
-        for(int i = 0; i < this.coordinates.length; i++) {
-            result += (this.coordinates[i])^2;
+        for (int coordinate : this.coordinates) {
+            result += coordinate ^ 2;
         }
         return sqrt(result);
     }
 
     public void PrintVector() {
         System.out.print("Координаты вектора: ");
-        for(int i = 0; i < this.coordinates.length; i++) {
-            System.out.printf("%d ", this.coordinates[i]);
+        for (int coordinate : this.coordinates) {
+            System.out.printf("%d ", coordinate);
         }
         System.out.println();
     }
